9d3c5913e3927da53055d6739656a1a0f5b8a878
RamziddinDeveloper <tekstipesen@gmail.com>
2022-04-16 06:29:41 +0500

    yangilandi

---
+++ 14_dars.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 16 05:56:58 2022
+
+@author: user
+"""
+
+# otam (onam, akam, ukam, va hokazo) degan lug'at yarating va lug'atga shu inson 
+# haqida kamida 3 ta m'alumot kiriting (ismi, tu'gilgan yili, shahri, manzili va hokazo). 
+# Lug'atdagi ma'lumotni matn shaklida konsolga chiqaring: Otamning ismi Mavlutdin, 1954-yilda, 
+# Samarqand viloyatida tug'ilgan
+
+# akam = {"ismi":"Abdulvahed", "yil":"1983", "manzili":"Yunusobod tumani"}
+# akam2 = {"ismi":"Fazliddin", "yil":"1985", "manzili":"Yunusobod tumani"}
+# ukam = {"ismi":"Zuhriddin", "yil":"1997", "manzili":"Yunusobod tumani"}
+
+
+# print(f"Kotta akamning ismi {akam['ismi']}, {akam['yil']}-yilda {akam['manzili']}da tug'ilgan")
+# print(f"Kichik akamning ismi {akam2['ismi']}, {akam2['yil']}-yilda {akam2['manzili']}da tug'ilgan")
+# print(f"Ukamning ismi {ukam['ismi']}, {ukam['yil']}-yilda {ukam['manzili']}da tug'ilgan")
+
+
+# otam = {'ismi':'mavlutdin', 'tyil':1954,'viloyat':'samarqand'}
+# tyil = otam['tyil']
+# vil = otam['viloyat']
+# print(f"Otamning ismi {otam['ismi'].title()}, {tyil}-yilda, {vil.title()} viloyatida tug'ilgan")
+
+
+# Oila a'zolaringizning sevimli taomlari lug'atini tuzing. Lug'atda kamida 5 ta ism-taom 
+# jufltigi bo'lsin. Kamida uch kishining sevimli taomini konsolga chiqaring: Alining 
+# sevimli taomi osh
+
